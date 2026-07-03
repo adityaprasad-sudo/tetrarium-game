8ec62deaa8b63efd85a3fd8684fc7987d15e2dbf
Aditya Prasad <adityaprasad1440@gmail.com>
2026-07-03 23:06:27 +0530

    -added some ui changes(in gaem) -fixed some bugs fixes #1 -also added the sprintingg mechanism and stamina bar -working on health rn

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,11 @@
 const mainmenu = document.getElementById('menu')
+const storyscr = document.getElementById('storyscr')
 const settingsmenu = document.getElementById('settingmenu')
 const loaidngcr = document.getElementById('loadingscr')
 const hud1 = document.getElementById('hud')
+const aarange = document.getElementById('aarange')
+const aaval = document.getElementById('aaval')
+
 const btnplay = document.getElementById('btnplay')
 const btnsettings = document.getElementById('settings')
 const backbtnsett = document.getElementById('backbtn')
@@ -10,8 +14,16 @@ btnplay.addEventListener('click', () =>{
     loaidngcr.style.display = 'flex'
     setTimeout(() => {
         loaidngcr.style.display = 'none'
-        hud1.style.display = 'flex'
-        if(typeof window.startgame === 'function') window.startgame()
+        storyscr.style.display = 'flex'
+        const startgm = (e) =>{
+            e.preventDefault()
+            window.removeEventListener('keydown', startgm)
+            storyscr.style.display = 'none'
+            hud1.style.display = 'block'
+            document.body.requestPointerLock()
+            if(typeof window.startgame === 'function') window.startgame()
+        }
+        window.addEventListener('keydown', startgm)
     }, 5000)
 })
 btnsettings.addEventListener('click', () => {
@@ -29,6 +41,8 @@ window.gamesettings = {
     resolution: 100,
     renderdistance: 1000,
     bloomintensity: 0.5,
+    aa: 2,
+    bloom: true
 }
 document.getElementById('mousesensin').addEventListener('input', (e) => {
     document.getElementById('mousesens').innerText = e.target.value
@@ -54,4 +68,34 @@ document.getElementById('bloomrange').addEventListener('input', (e) => {
     document.getElementById('bloomval').innerText = e.target.value
     window.gamesettings.bloomintensity = parseFloat(e.target.value)
     if(typeof window.updategraph === 'function') window.updategraph(window.gamesettings)
+})
+const paumenu = document.getElementById('pausemenu')
+const btnresume = document.getElementById('btnresume')
+const btnquit = document.getElementById('btnquit')
+document.addEventListener('pointerlockchange', () => {
+    if(typeof gamestart !== 'undefined' &&  gamestart === true){
+        if(document.pointerLockElement === document.body){
+            paumenu.style.display = 'none'
+        }else{
+            paumenu.style.display = 'flex'
+        }
+    }
+})
+btnresume.addEventListener('click', () => {
+    document.body.requestPointerLock()
+})
+btnquit.addEventListener('click', () => {
+    window.location.reload()
+})
+aarange.addEventListener('input', () => {
+    const val = parseInt(aarange.value)
+    if(val === 0) aaval.innerText = 'OFF'
+    else aaval.innerText = Math.pow(2,val) + "x"
+    window.gamesettings.aa = val
+    if(typeof window.updategraph === 'function') window.updategraph(window.gamesettings)
+})
+const bloomchk = document.getElementById('bloomcheck')
+bloomchk.addEventListener('change', (e) => {
+    window.gamesettings.bloom = e.target.checked
+    if(typeof window.updategraph === 'function'){ window.updategraph(window.gamesettings)}
 })
